88482d7fb4b5fcb564da3ec68f7f5a5e3622906f
pineqcorn <146956076+pineqcorn@users.noreply.github.com>
2023-11-18 16:52:02 -0500

    added control panel

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,11 +15,13 @@ public class Main {
 				board[row][col] = '-';
 			}
 		}
-		// TODO Auto-generated method stub
-		//Load.loading(20, 1000, 1);
-	//	System.out.println(play_game(board, 'x', new Vertex(board)));
-		//System.out.println(boardGraph);
-		//System.out.print(boardGraph.getEdges().get(0).getParent().getOutEdges().get(0).getChild().getOutEdges());
+		play_game(board, 'x', new Vertex(board));
+		Scanner scan = new Scanner(System.in);
+		
+		ArrayList<Vertex> dummy = new ArrayList<Vertex>();
+		dummy.add(boardGraph.getEdges().get(0).getParent());
+		
+		controlPanel(dummy, scan);
 	}
 	
 	private static int play_game(Character[][] board, Character move, Vertex parent) {
@@ -94,6 +97,103 @@ public class Main {
 				return board[1][1] + " win";
 			}
 		return "no win";
+	}
+	
+	private static void controlPanel(ArrayList<Vertex> vArray, Scanner scan) {
+		for (int i = 0; i < vArray.size(); i++) {
+			if (i == 0) {
+				System.out.println("* " + vArray.get(i) + " ROOT");
+				continue;
+			}
+			else if (i == vArray.size() - 1) {
+				System.out.println("v " + vArray.get(i));
+				continue;
+			}
+			System.out.println("| " + vArray.get(i));
+		}
+		Vertex v = vArray.get(vArray.size() - 1);
+		System.out.println("Current Vertex: " + v);
+		
+		if (v.getInEdges().size() > 0) {
+			System.out.println("\nIn Edges");
+			System.out.println("\n#  parent    child     value");			
+			//prints out all in edges
+			for (Edge e : v.getInEdges()) {
+				System.out.println(v.getInEdges().indexOf(e) + ") " + e);
+			}
+		} else {
+			System.out.println("There are no incoming edges");
+		}
+		
+		if (v.getOutEdges().size() > 0) {
+			System.out.println("\nOut Edges");
+			System.out.println("\n#  parent    child     value");	
+			
+			//prints out all out edges
+			for (Edge e : v.getOutEdges()) {
+				System.out.println(v.getOutEdges().indexOf(e) + ") " + e);
+			}
+		} else {
+			System.out.println("There are no outgoing edges");
+		}
+		
+		//choose in/out edges
+		int ioChoice = 0;
+		final int IN_EDGES = 1;
+		final int OUT_EDGES = 2;
+		
+		do {
+			System.out.print("\n1) In edges\n2) Out edges\n");
+			ioChoice = scan.nextInt();
+			if (ioChoice > 2 || ioChoice < 0) {
+				System.out.println("Invalid input");
+			}
+			
+			if (v.getInEdges().size() <= 0 && ioChoice == IN_EDGES) {
+				System.out.println("There are no incoming edges");
+			}
+			
+			if (v.getOutEdges().size() <= 0 && ioChoice == OUT_EDGES) {
+				System.out.println("There are no outgoing edges");
+			}
+		} while (ioChoice > 2 || ioChoice < 0 || (v.getInEdges().size() <= 0 && ioChoice == IN_EDGES) || (v.getOutEdges().size() <= 0 && ioChoice == OUT_EDGES));
+		
+		
+		ArrayList<Edge> EDGE_ARRAY = null;
+		if (ioChoice == 1) {EDGE_ARRAY = v.getInEdges();}
+		else if (ioChoice == 2) {EDGE_ARRAY = v.getOutEdges();}
+		
+		
+		//choose edge in specified in/out group
+		int edgeChoice = 0;
+		do {
+			System.out.print("Choose an edge number: ");
+			edgeChoice = scan.nextInt();
+			if (edgeChoice > EDGE_ARRAY.size() || edgeChoice < 0) {
+				System.out.println("Invalid input");
+			}
+		} while (edgeChoice > EDGE_ARRAY.size() || edgeChoice < 0);
+		
+		
+		//in edges go to parent while out edges go to child
+		if (ioChoice == 1) {vArray.add(EDGE_ARRAY.get(edgeChoice).getParent());}
+		else if (ioChoice == 2) {vArray.add(EDGE_ARRAY.get(edgeChoice).getChild());}
+		
+		//System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+		System.out.flush();
+		
+		controlPanel(vArray, scan);
+		
+		
+		
+		
+		
+		//start at root
+			//print out out/in edges
+			//select o/i
+			//select number
+			//print out children and parents
+			//repeat
 	}
 
 }
